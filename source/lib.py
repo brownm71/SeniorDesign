952a13fe90fdc2b85a03463b698f279bb8048ee0
Crime188 <crimj@miamioh.edu>
2025-09-12 23:45:14 -0400

    Added Basic Interpo.

--- source/lib.py
+++ source/lib.py
@@ -12,7 +12,7 @@ class Player:
         self.name = name
         for time in player_dict.keys():
             # Do things with time????
-            self.times_to_pos[time] = player_dict[time]
+            self.times_to_pos[float(time)] = player_dict[time]
     
     def toDict(self):
         result = {}
@@ -20,7 +20,70 @@ class Player:
             # do things with time??
             result[time] = self.times_to_pos[time]
         return result
-            
+    
+    def basicInterpolateFill(self, timestep = 1):
+        """Take all the points, and fill in missing ones using Interpolation."""
+        # assumes Initial positions are accurate and there.
+
+        sorted_times = sorted(self.times_to_pos.keys())
+        start_time = float(sorted_times[0])
+        end_time = float(sorted_times[-1])
+
+        current_time = start_time
+        while current_time <= end_time:
+            if current_time not in self.times_to_pos:
+                # Add a placeholder for the missing time
+                self.times_to_pos[current_time] = {'LOCATION': None, 'CONFIDENCE': -1.0}
+            current_time += timestep
+
+        # make sure the times are sorted.
+        sorted_times = sorted(self.times_to_pos.keys())
+        points = []
+        for time in sorted_times:
+            confidence = self.times_to_pos[time]['CONFIDENCE']
+            if confidence < 0:
+                # this is a point to fill in.
+                points.append(None)
+            else:
+                points.append(self.times_to_pos[time]['LOCATION'])
+        # we have collected the points, now we fill in the gaps
+        for i in range(len(points)):
+            if points[i] is None:
+                # This is a point to fill in.
+                # find the closest point to the left.
+                left_index = -1
+                for j in range(i - 1, -1, -1):
+                    if points[j] is not None:
+                        left_index = j
+                        break
+                # find the closest point to the right.
+                right_index = -1
+                for j in range(i + 1, len(points)):
+                    if points[j] is not None:
+                        right_index = j
+                        break
+                # if we have a point on both sides, we can interpolate
+                if left_index != -1 and right_index != -1:
+                    left_point = points[left_index]
+                    right_point = points[right_index]
+
+                    total_steps = float(right_index - left_index)
+                    current_step = float(i - left_index)
+                    # Assuming LOCATION is [x, y] currently
+                    if isinstance(left_point, list) and isinstance(right_point, list):
+                        interpolated_point = []
+                        for dim in range(len(left_point)): # so that any number of dimentions can be used.
+                            val = left_point[dim] + (right_point[dim] - left_point[dim]) * (current_step / total_steps)
+                            interpolated_point.append(val)
+
+                        points[i] = list(interpolated_point)
+
+        # Now we update the original dictionary with the interpolated points
+        for i, time in enumerate(sorted_times):
+            if points[i] is not None and self.times_to_pos[time]['CONFIDENCE'] < 0:
+                self.times_to_pos[time]['LOCATION'] = points[i]
+                self.times_to_pos[time]['CONFIDENCE'] = 0.5 # Mark as interpolated 
+
 class Team:
     """A List of Players"""
     list_of_players :list[Player]
---
+++ source/testing_Interpolate.py
@@ -0,0 +1,15 @@
+import time
+from lib import *
+from fileRead import *
+meta,teams = readJson("SeniorDesign/docs/singleTeamTest.json")
+# print(teams)
+teams : list[Team]
+for team in teams:
+    for player in team.list_of_players:
+        # print(player.times_to_pos)
+        player.basicInterpolateFill(1)
+        # print((player.times_to_pos))
+
+meta['date'] = f'{time.localtime()[1]}/{time.localtime()[2]}/{time.localtime()[0]}' # Cursed way to change the metaData Time
+# print(meta)
+writeJson('TestOut.json',teams,meta)
